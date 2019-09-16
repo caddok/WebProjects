878e1fd5b52b9cc3a04d81d44cf067e42754a1f9
Georgi-Delchev <georgi.delchev@adastragrp.com>
2019-09-16 12:41:08 +0300

    set up for Chattrbox

---
+++ Chattrbox/index.js
@@ -0,0 +1,19 @@
+var http = require("http");
+var fs = require("fs");
+var extract = require("./extract");
+var wss = require('./websockets-server');
+
+var server = http.createServer(function(req, res) {
+  console.log("Responding to a request.");
+  var filePath = extract(req.url);
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    }
+  });
+});
+server.listen(3000);
