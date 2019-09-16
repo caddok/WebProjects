d03e98d9624c5e24129789484be5c80d7b78a3ce
Georgi-Delchev <georgi.delchev@adastragrp.com>
2019-09-16 16:51:30 +0300

    babel setup

--- Chattrbox/index.js
+++ Chattrbox/index.js
@@ -1,6 +1,25 @@
-var http = require('http');
-var server = http.createServer(function (req, res) {
- console.log('Responding to a request.');
- res.end('<h1>Hello, World</h1>');
+var http = require("http");
+var fs = require("fs");
+var extract = require("./extract");
+var wss = require('./websockets-server');
+
+var handleError = function(err, res) {
+  res.writeHead(404);
+  res.end();
+};
+
+var server = http.createServer(function(req, res) {
+  console.log("Responding to a request.");
+  var filePath = extract(req.url);
+
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      handleError(err, res);
+      return;
+    } else {
+      res.setHeader("Content-Type", "text/html");
+      res.end(data);
+    }
+  });
 });
 server.listen(3000);
